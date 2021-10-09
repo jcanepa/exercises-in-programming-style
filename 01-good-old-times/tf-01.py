f18e4a92815b64487eacbd9554620dbcca48a228
J. Canepa <julian.l.canepa@gmail.com>
2021-10-08 22:52:30 -0700

    Update shebang for local execution.

--- 01-good-old-times/tf-01.py
+++ 01-good-old-times/tf-01.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/local/bin/python3
 import sys, os, string
 
 # Utility for handling the intermediate 'secondary memory'
@@ -13,18 +13,18 @@ def touchopen(filename, *args, **kwargs):
 # The constrained memory should have no more than 1024 cells
 data = []
 # We're lucky:
-# The stop words are only 556 characters and the lines are all 
-# less than 80 characters, so we can use that knowledge to 
-# simplify the problem: we can have the stop words loaded in 
+# The stop words are only 556 characters and the lines are all
+# less than 80 characters, so we can use that knowledge to
+# simplify the problem: we can have the stop words loaded in
 # memory while processing one line of the input at a time.
-# If these two assumptions didn't hold, the algorithm would 
+# If these two assumptions didn't hold, the algorithm would
 # need to be changed considerably.
 
-# Overall strategy: (PART 1) read the input file, count the 
-# words, increment/store counts in secondary memory (a file) 
+# Overall strategy: (PART 1) read the input file, count the
+# words, increment/store counts in secondary memory (a file)
 # (PART 2) find the 25 most frequent words in secondary memory
 
-# PART 1: 
+# PART 1:
 # - read the input file one line at a time
 # - filter the characters, normalize to lower case
 # - identify words, increment corresponding counts in file
@@ -48,13 +48,13 @@ word_freqs = touchopen('word_freqs', 'rb+')
 f = open(sys.argv[1], 'r')
 # Loop over input file's lines
 while True:
-    data[1] = [f.readline()] 
+    data[1] = [f.readline()]
     if data[1] == ['']: # end of input file
         break
     if data[1][0][len(data[1][0])-1] != '\n': # If it does not end with \n
         data[1][0] = data[1][0] + '\n' # Add \n
     data[2] = None
-    data[3] = 0 
+    data[3] = 0
     # Loop over characters in the line
     for c in data[1][0]: # elimination of symbol c is exercise
         if data[2] == None:
@@ -64,7 +64,7 @@ while True:
         else:
             if not c.isalnum():
                 # We found the end of a word. Process it
-                data[4] = False 
+                data[4] = False
                 data[5] = data[1][0][data[2]:data[3]].lower()
                 # Ignore words with len < 2, and stop words
                 if len(data[5]) >= 2 and data[5] not in data[0]:
@@ -75,7 +75,7 @@ while True:
                             break;
                         data[7] = int(data[6].split(',')[1])
                         # word, no white space
-                        data[6] = data[6].split(',')[0].strip() 
+                        data[6] = data[6].split(',')[0].strip()
                         if data[5] == data[6]:
                             data[7] += 1
                             data[4] = True
@@ -114,10 +114,10 @@ while True:
     # Check if this word has more counts than the ones in memory
     for i in range(25): # elimination of symbol i is exercise
         if data[i] == [] or data[i][1] < data[26]:
-            data.insert(i, [data[25], data[26]]) 
+            data.insert(i, [data[25], data[26]])
             del data[26] #  delete the last element
             break
-            
+
 for tf in data[0:25]: # elimination of symbol tf is exercise
     if len(tf) == 2:
         print(tf[0], '-', tf[1])
